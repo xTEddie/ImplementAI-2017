f95775c34b132b3ff826d143915dd651a19787bb
xTEddie <edwardtranv1@gmail.com>
2017-09-30 00:08:08 -0400

    Add basic state

--- src/component/Home/index.js
+++ src/component/Home/index.js
@@ -4,12 +4,19 @@ import React, {Component} from 'react';
 
 class Home extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: 'hi'
+        }
+    }
 
     render() {
 
         return (
             <div> 
                 <h1> HELLO WORLD </h1>
+                <h2> {this.state.message} </h2>
             </div>
         )
     }
