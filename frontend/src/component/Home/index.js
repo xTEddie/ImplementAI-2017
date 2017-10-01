9561966b2d1a6681f55a27ce85e9b8992f4df0ee
xTEddie <edwardtranv1@gmail.com>
2017-10-01 05:10:30 -0400

    Fix bug with logout

--- frontend/src/component/Home/index.js
+++ frontend/src/component/Home/index.js
@@ -165,9 +165,10 @@ class Home extends Component {
            <button
                 className="btn btn-lg btn-custom"
                 onClick={() => {                                            
-                    window.FB.logout((response) => {
-                        this.setState({loggedIn: false});
-                    });
+                    window.FB.logout((response) => {  
+                        console.log(response);                                         
+                        window.location.href = '/';                        
+                    })
                 }}
             >
                 Logout
