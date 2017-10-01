031166a5bff5a31a74b4568372ea2166e09a4d18
xTEddie <edwardtranv1@gmail.com>
2017-10-01 07:14:38 -0400

    Add footer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter as Router, Route} from 'react-router-dom';
 import Home from './component/Home';
+import Footer from './component/Footer';
 
 
 ReactDOM.render(
     <Router>
         <div>
             <Route exact path="/" component={Home}/>                                
+            <Route path="/" component={Footer}/>  
         </div>
     </Router>,
     document.getElementById('root')
